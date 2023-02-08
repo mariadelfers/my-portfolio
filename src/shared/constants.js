ce0470b38e352f1893cf6800ae2c2c5d64b58756
María F Hernández <33531001+mariadelfers@users.noreply.github.com>
2023-02-07 22:14:24 -0600

    feat: constants added

---
+++ src/shared/constants.js
@@ -0,0 +1,5 @@
+export const URLS_CONSTANTS = {
+    GITHUB: 'https://github.com/mariadelfers',
+    LINKEDIN: 'https://www.linkedin.com/in/mar%C3%ADa-fernanda-hern%C3%A1ndez-8a2098182/',
+    YOUTUBE: 'https://www.youtube.com/@mariadelfers',
+};
