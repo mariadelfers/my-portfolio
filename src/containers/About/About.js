d85c40110ac7104068a7ca63d489b2e003afdfc1
María F Hernández <33531001+mariadelfers@users.noreply.github.com>
2023-02-10 21:48:30 -0600

    refactor: about container

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,32 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Particle from "../../components/core/Particle";
-import Github from "../../components/about/Github";
+import { Container } from "react-bootstrap";
 import Techstack from "../../components/about/Techstack";
-import Aboutcard from "../../components/about/AboutCard";
-import laptopImg from "../../assets/about.png";
 import Toolstack from "../../components/about/Toolstack";
 import Info from "../../components/about/Info";
 
 function About() {
   return (
     <Container fluid className="about-section">
-      <Particle />
       <Container>
-       
         <Info />
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          My <strong className="purple">Skills </strong>
         </h1>
-
         <Techstack />
-
         <h1 className="project-heading">
-          <strong className="purple">Tools</strong> I use
+          <strong className="purple">Tools</strong>
         </h1>
         <Toolstack />
-
-        <Github />
       </Container>
     </Container>
   );
